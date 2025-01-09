86aed170aaf8de2fdb8f2d059e01df6544ce2b2a
James Smith <james@floppy.org.uk>
2025-01-09 17:11:41 +0000

    check that base mesh is loaded correctly

--- test/MANYFOLD_mesh_progressive.js
+++ test/MANYFOLD_mesh_progressive.js
@@ -20,19 +20,27 @@ export default QUnit.module('MANYFOLD_mesh_progressive', () => {
   });
 
   QUnit.module('GLTFManyfoldMeshProgressiveExtension-webonly', () => {
-    QUnit.test('parse', assert => {
+    QUnit.test('loads base mesh', assert => {
       const done = assert.async();
       const assetPath = '../examples/assets/gltf/Progressive/progressive.glb';
       new GLTFLoader()
         .register(parser => new GLTFManyfoldMeshProgressiveExtension(parser))
         .load(assetPath, gltf => {
           let hasBaseMesh = false;
+          let vertexCount = 0;
+          let faceCount = 0;
           gltf.scene.traverse(object => {
             if (object.isMesh) {
               hasBaseMesh = true;
+              if (object.geometry) {
+                faceCount = object.geometry.index.count
+                vertexCount = object.geometry.getAttribute("position").count
+              }
             }
           });
           assert.ok(hasBaseMesh, 'can parse base mesh');
+          assert.ok(vertexCount == 1647, 'loads expected number of vertices');
+          assert.ok(faceCount == 2460, 'loads expected number of faces');
           done();
         }, undefined, error => {
           assert.ok(false, 'can load base mesh');
